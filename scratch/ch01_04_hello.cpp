560a6521f86782c4ef02b8bdab512f488f2c5c1e
p1n9u <cybrphntm4@hanyang.ac.kr>
2023-04-13 15:34:45 +0900

    initial settings

---
+++ scratch/ch01_04_hello.cpp
@@ -0,0 +1,11 @@
+/* Declares objects that control reading from and writing to the standard streams. */
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+    /* Specifies the cout global stream. */
+    cout << "from sea to shining C\n";
+    return 0;
+}
